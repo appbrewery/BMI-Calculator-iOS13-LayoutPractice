0a025367aee166e590b0a0779db74c807fadae5e
Angela <8798027+angelabauer@users.noreply.github.com>
2019-08-21 12:38:30 +0100

    Initial Commit

---
+++ BMI-Calculator-LayoutPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BMI-Calculator-LayoutPractice
+//
+//  Created by Angela Yu on 21/08/2019.
+//  Copyright © 2019 App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
